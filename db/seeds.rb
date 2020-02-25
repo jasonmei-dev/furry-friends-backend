bdea3321765d796397df104e69975f849d1f0124
Jason Mei <jason.mei403@gmail.com>
2020-02-25 17:19:44 -0500

    create test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'Jason', last_name: 'Mei', email: 'jmei403@yahoo.com', password: 'password', city: 'Staten Island', state: 'New York', country: 'United States', postcode: 10304)
