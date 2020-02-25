fd05d000163f9c30bc874be5fb12c3c9b8cb9ccd
Jason Mei <jason.mei403@gmail.com>
2020-02-25 17:20:26 -0500

    namespaced controllers and routes under api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :lists
-  resources :favorited_pets
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :lists
+      resources :favorited_pets
+      resources :users
+    end
+   end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
