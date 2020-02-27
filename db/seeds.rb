7454c6b5df73393314b90d534cf32163c2893f05
Jason Mei <jason.mei403@gmail.com>
2020-02-26 19:17:59 -0500

    add test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: 'Jason', last_name: 'Mei', email: 'jmei403@yahoo.com', password: 'password', city: 'Staten Island', state: 'New York', country: 'United States', postcode: 10304)
+jason = User.create(first_name: 'Jason', last_name: 'Mei', email: 'jmei403@yahoo.com', password: 'password', city: 'Staten Island', state: 'New York', country: 'United States', postcode: 10304)
+
+helen = User.create(first_name: 'Helen', last_name: 'Shiu', email: 'helennndx@hotmail.com', password: 'password', city: 'Rego Park', state: 'New York', country: 'United States', postcode: 11374)
