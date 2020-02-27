eda24451fb04dd0a87664b64c3bdb976052f4ff1
Jason Mei <jason.mei403@gmail.com>
2020-02-26 19:17:40 -0500

    update association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_one :list
-  has_many :favorited_pets, through: :list
+  has_many :lists
+  has_many :favorited_pets, through: :lists
 end
