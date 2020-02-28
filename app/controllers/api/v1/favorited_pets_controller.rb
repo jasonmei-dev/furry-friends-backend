19a8d4773c72183cb9401d1e02855ab3a3979385
Jason Mei <jason.mei403@gmail.com>
2020-02-28 15:34:21 -0500

    add basic create action

--- app/controllers/api/v1/favorited_pets_controller.rb
+++ app/controllers/api/v1/favorited_pets_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::FavoritedPetsController < ApplicationController
+  def create
+    pet_id = params[:id] # id from API
+    favorited_pet = FavoritedPet.create(api_id: pet_id)
+    render json: favorited_pet
+  end
 end
