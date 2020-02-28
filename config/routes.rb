be50df3ade68937b8edf8f6c3527715e3a6b7fc8
Jason Mei <jason.mei403@gmail.com>
2020-02-28 16:29:27 -0500

    update routes for pets and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
-      resources :lists
-      resources :favorited_pets
       resources :users
-      get '/pets', to: 'petfinder#index'
-      get '/pets/:id', to: 'petfinder#pet_details'
+      resources :likes
+      resources :pets
+      get '/petfinder', to: 'petfinder#index'
+      get '/petfinder/:id', to: 'petfinder#pet_details'
       get '/:type', to: 'petfinder#get_type'
     end
    end
