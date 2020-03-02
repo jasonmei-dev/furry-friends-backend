0d73ff03db2dbe2cd94b17684c264291ad12f0ec
Jason Mei <jason.mei403@gmail.com>
2020-03-02 15:15:55 -0500

    update test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 jason = User.create(first_name: 'Jason', last_name: 'Mei', email: 'jmei403@yahoo.com', password: 'password', city: 'Staten Island', state: 'New York', country: 'United States', postcode: 10304)
-
 helen = User.create(first_name: 'Helen', last_name: 'Shiu', email: 'helennndx@hotmail.com', password: 'password', city: 'Rego Park', state: 'New York', country: 'United States', postcode: 11374)
+
+bailey = Pet.create(pet_api_id: 47409516)
+slippers = Pet.create(pet_api_id: 47402188)
+
+Like.create(user_id: jason.id, pet_id: bailey.id)
+Like.create(user_id: jason.id, pet_id: slippers.id)
+Like.create(user_id: helen.id, pet_id: slippers.id)
