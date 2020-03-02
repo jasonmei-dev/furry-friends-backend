d516b7ca33a6993d41fa9d12d964146970af66af
Jason Mei <jason.mei403@gmail.com>
2020-03-02 15:13:15 -0500

    namespace likes and pets

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
       resources :users
       resources :likes
       resources :pets
-      get '/petfinder', to: 'petfinder#index'
-      get '/petfinder/:id', to: 'petfinder#pet_details'
-      get '/:type', to: 'petfinder#get_type'
+      # get '/petfinder', to: 'petfinder#index'
+      # get '/petfinder/:id', to: 'petfinder#pet_details'
+      get '/:type', to: 'pets#get_type'
     end
    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
