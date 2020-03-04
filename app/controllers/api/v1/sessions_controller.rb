14f0c67d0ea024c6afc061d7e8cb293c0a7a53be
Jason Mei <jason.mei403@gmail.com>
2020-03-03 23:20:11 -0500

    update logout

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,18 +14,14 @@ class Api::V1::SessionsController < ApplicationController
   end
 
   def get_current_user
-    if logged_in?
-      render json: current_user
-    else
-      render json: {error: "Not logged in" }, status: :unauthorized
-    end
+    render json: current_user
   end
 
   def destroy
     session.clear
     render json: { notice: "Sucessfully logged out" }, status: :ok
   end
-  
+
   private
 
   def session_params
