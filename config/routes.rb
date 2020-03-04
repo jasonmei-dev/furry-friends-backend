0bb81d1e55425561b811a70340a04bfe797ad929
Jason Mei <jason.mei403@gmail.com>
2020-03-03 20:20:15 -0500

    update for get_current_user

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
       resources :pets
       # get '/petfinder', to: 'petfinder#index'
       # get '/petfinder/:id', to: 'petfinder#pet_details'
-      get '/:type', to: 'pets#get_type'
+      # get '/:type', to: 'pets#get_type'
       post '/login', to: 'sessions#create'
+      get '/get_current_user', to: 'sessions#get_current_user'
     end
    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
