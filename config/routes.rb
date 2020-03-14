c8c9b3f2fb567dec8886dac42db53f9cf1a08c15
Jason Mei <jason.mei403@gmail.com>
2020-03-13 23:01:08 -0400

    add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       post '/login', to: 'sessions#create'
       get '/get_current_user', to: 'sessions#get_current_user'
       delete '/logout', to: 'sessions#destroy'
+      post '/signup', to: 'users#create'
     end
    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
