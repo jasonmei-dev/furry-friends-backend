bfbcfbdc5bbe4cf930a1e5793807ed83e0523263
Jason Mei <jason.mei403@gmail.com>
2020-03-13 23:00:53 -0400

    add user signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,35 @@ class Api::V1::UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+  def create #signup
+    # byebug
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      resp = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :city,
+      :state,
+      :country,
+      :postcode
+    )
+  end
+
 end
