2f702319ceb9bacf3e38bc628df07b438010eab0
Jason Mei <jason.mei403@gmail.com>
2020-04-16 13:50:33 -0400

    add create action

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -3,4 +3,34 @@ class Api::V1::LikesController < ApplicationController
     myPets = current_user.pets
     render json: myPets
   end
+
+  def create
+    if Pet.find_by(pet_api_id: params[:pet][:id])
+      petId = Pet.find_by(pet_api_id: params[:pet][:id]).id
+
+      if current_user.likes.where(pet_id: petId).length > 0
+        render json: { error: 'Pet already favorited!' }
+      else
+        just_added = Like.create(user_id: current_user.id, pet_id: petId)
+        render json: just_added.pet
+      end
+
+    else
+      newPet = Pet.find_or_create_by(
+        pet_api_id: params[:pet][:id],
+        name: params[:pet][:name],
+        pet_type: params[:pet][:type],
+        species: params[:pet][:species],
+        age: params[:pet][:age],
+        gender: params[:pet][:gender],
+        size: params[:pet][:size],
+        breeds: params[:pet][:breeds],
+        photos: params[:pet][:photos],
+        contact: params[:pet][:contact]
+      )
+      just_added = Like.create(user_id: current_user.id, pet_id: newPet.id)
+      render json: just_added.pet
+    end
+  end
+
 end
