7d2f30b1cdc3dccdad433f0aa89d60b3040bc546
Jason Mei <jason.mei403@gmail.com>
2020-04-16 13:50:55 -0400

    update serialize

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,7 @@ class Pet < ApplicationRecord
   has_many :likes
   has_many :users, through: :likes
 
-  serialize :breeds, Hash
-  serialize :photos, Array
-  serialize :contact, Hash
+  serialize :breeds
+  serialize :photos
+  serialize :contact
 end
