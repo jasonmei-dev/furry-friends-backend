d02aa08b894212b46a4d2131276f05f63c4a7abc
Jason Mei <jason.mei403@gmail.com>
2020-04-22 15:15:59 -0400

    change get_type from GET to POST

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       resources :pets
       # get '/petfinder', to: 'petfinder#index'
       # get '/petfinder/:id', to: 'petfinder#pet_details'
-      # get '/:type', to: 'pets#get_type'
+      post '/get_type', to: 'pets#get_type'
       post '/login', to: 'sessions#create'
       get '/get_current_user', to: 'sessions#get_current_user'
       delete '/logout', to: 'sessions#destroy'
