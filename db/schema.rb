1ac52dc5a5657f9aca7b9d0506576232c59c4898
Jason Mei <jason.mei403@gmail.com>
2020-04-24 10:48:08 -0400

    update pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_004601) do
+ActiveRecord::Schema.define(version: 2020_04_23_150410) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_004601) do
     t.string "breeds"
     t.string "photos"
     t.string "contact"
+    t.string "primary_photo_cropped"
   end
 
   create_table "users", force: :cascade do |t|
