3e83ed2f3af8c3a22ef063208d358fae3f206bc0
Jason Mei <jason.mei403@gmail.com>
2020-05-11 11:59:27 -0400

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
 
   has_many :likes
   has_many :pets, through: :likes
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+  validates :postcode, presence: true
 end
