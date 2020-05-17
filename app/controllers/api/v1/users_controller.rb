058512f9f551d39a2f8feb61c8a2e4046fb2d91b
Jason Mei <jason.mei403@gmail.com>
2020-05-17 14:42:11 -0400

    user update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,25 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create #signup
-    # byebug
     user = User.new(user_params)
 
     if user.save
       session[:user_id] = user.id
       render json: UserSerializer.new(user), status: :created
+    else
+      resp = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    # byebug
+    user = User.find_by(id: params[:id])
+
+    if user.update(update_params)
+      render json: UserSerializer.new(user)
     else
       resp = {
         error: user.errors.full_messages.to_sentence
@@ -27,6 +40,18 @@ class Api::V1::UsersController < ApplicationController
       :last_name,
       :email,
       :password,
+      :city,
+      :state,
+      :country,
+      :postcode
+    )
+  end
+
+  def update_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
       :city,
       :state,
       :country,
