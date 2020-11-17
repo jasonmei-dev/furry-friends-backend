ef7d0a7733d31ed4bac2620f3979e40846ab0263
jmei403 <jason.mei403@gmail.com>
2020-11-17 16:14:00 -0500

    CORS debugging

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://furry-friends-app.herokuapp.com/'
+    origins 'http://localhost:3000', 'https://furry-friends-app.herokuapp.com'
 
     resource '*',
       headers: :any,
